3c36575cb630bde893ff325e6ed1a3f545fb11a3
Max Juhtanovs <juhtanovs.m@gmail.com>
2023-08-23 17:10:38 +0100

    EOD commit
    almost got the blinking timer and mode dependency going but needs tidying up

--- Receiver_CAN/CANcillary/CANcillary.ino
+++ Receiver_CAN/CANcillary/CANcillary.ino
@@ -10,12 +10,39 @@
 #define FLSENSE 12            // SW1 input
 #define FRSENSE 6             // SW2 input
 #define CH3ADC A1         /// Horn signal 
-#define IND_L_SENSE 4     // Left indicator
-#define IND_R_SENSE 21    // Right indicator
-//#define HEADLIGHTS 5      // Headlights input
+#define IND_L_SENSE 4     // Left indicator - 2 in dbc - red on breadboard
+#define IND_R_SENSE 21    // Right indicator - 3 in dbc - green on breadboard
+#define HEADLIGHTS 5      // Headlights input
 #define PROGRAM_MODE false     // set this to enable the serial port (the code will not run if serial enabled but not connected)
 
 MCP_CAN CAN(SPI_CS_PIN);            // Set CS pin
+
+//Messages of interest
+byte VehicleStateIndicators[8];
+byte ByWireControlIndicators[8];
+byte DriveByWireState[8];
+
+//Timers
+const long blinkInterval = 400;  // Blink interval in milliseconds
+const unsigned long frameRepetitionTime=1000;  // repetition rate of the frame in ms  SET TO 1s for INITIAL TESTS
+unsigned long previousMillis = 0;
+unsigned long previousTime;                    // last time for frame rate calcs
+byte count = 0;  // rolling counter for alive signal
+uint16_t ms = 0;
+int indicator_timer_cal = 400; //This Cal controls how frequently the indicators pulse 
+//uint16_t can_rc_time_ms_104 = 0; 
+//uint16_t can_rc_time_ms_100 = 0;
+//int can_error_timeout_cal = 200;
+
+//Switches
+bool leds = true;
+bool Autonomous_Mode = false;
+bool Indicator_Switch = false;
+
+//Preset int values
+unsigned int CH3Input = 0;
+unsigned int indicatorcommand = 0;
+
 
 void setup()
 {
@@ -51,49 +78,19 @@ void setup()
   CAN.init_Filt(2, 0, 0x1A0);
 }
 
-//JOYSTICK LEGACY STUFF
-//int SteerRawValue = 0;  // raw value of steer pot
-//int ThroRawValue = 0;   // raw value of throttle pot
-
-//const unsigned int Steer_X[5] = {0x00, 0x1A0, 0x1EA, 0x210, 0x3A5};    //Arduino has 10 bit ADC so 0->2FF
-//const int  Steer_Y[5] =         {-100,   0,      0,      0,      100};
-
-//const unsigned int Torque_X[5] = {0xE1, 0x1B0, 0x1E5, 0x210, 0x3A5};
-//const int  Torque_Y[5] =        {-100,   0,      0,      0,      100};
-
-const unsigned long frameRepetitionTime=1000;  // repetition rate of the frame in ms  SET TO 1s for INITIAL TESTS
-unsigned long previousTime;                    // last time for frame rate calcs
-
-byte VehicleStateIndicators[8];
-byte ByWireControlIndicators[8];
-byte DriveByWireState[8];
-
-byte count = 0;  // rolling counter for alive signal
-bool leds = true;
-bool Autonomous_Mode = false;
-unsigned int CH3Input = 0;
-unsigned int indicatorcommand = 0;
-uint16_t ms = 0;
-
-
-//This Cal controls how frequently the indicators pulse 
-int indicator_timer_cal = 400;
-uint16_t can_rc_time_ms_104 = 0;
-uint16_t can_rc_time_ms_100 = 0;
-//int can_error_timeout_cal = 200;
-
 
 void loop()
 {
-  if(0 == (ms % indicator_timer_cal)){
-		if (Indicator_High_B){
-			Indicator_High_B = false;
-		}else{
-			Indicator_High_B = true;
-		}
-	}
-  //digitalWrite(THROADC, LOW);
-  unsigned long currentTime = millis();   // grab the current time
+  unsigned long currentTime = millis();
+  if (currentTime - previousMillis >= blinkInterval) {
+    previousMillis = currentTime;  
+      if (digitalRead(Indicator_Switch) == HIGH) {
+          digitalWrite(Indicator_Switch, LOW);
+        } 
+      else {
+          digitalWrite(Indicator_Switch, HIGH);
+        }
+    }
 
   if (currentTime-previousTime >= frameRepetitionTime) {
     digitalWrite(LED,leds);  // light/extinguish the led on alternate cycles
@@ -138,33 +135,22 @@ void loop()
         unsigned int combinedvalue;
         combinedvalue = (highByte << 8 )| lowByte;
 
-        if(Autonomous_Mode == true){
-          if (indicatorcommand == 2) {
+        if(Autonomous_Mode == true && Indicator_Switch == true && indicatorcommand == 2 ){
             digitalWrite(IND_L_CONT, HIGH);
             digitalWrite(IND_R_CONT, LOW);
-            //delay(indicator_timer_cal);
-            } 
-          else if (indicatorcommand == 3){
+          } 
+        if (Autonomous_Mode == true && Indicator_Switch == true && indicatorcommand == 3){
             digitalWrite(IND_L_CONT, LOW);
             digitalWrite(IND_R_CONT, HIGH);
-            //delay(indicator_timer_cal);
-            } 
-          else if (indicatorcommand == 4){
+          } 
+        if (Autonomous_Mode == true && Indicator_Switch == true && indicatorcommand == 4){
             digitalWrite(IND_L_CONT, HIGH);
             digitalWrite(IND_R_CONT, HIGH);
-            //delay(indicator_timer_cal);
-            } 
-          else{
-            digitalWrite(IND_L_CONT, LOW);
-            digitalWrite(IND_R_CONT, LOW);
-            //delay(indicator_timer_cal);
-            }
-          }
+          } 
         else{digitalWrite(IND_L_CONT, LOW);
             digitalWrite(IND_R_CONT, LOW);
-            //delay(indicator_timer_cal);
-            } // Auto mode dependancy
-        } // End of message 113
+            } // End of actions on 0x113 signals
+        } // End of message 0x113 extraction
 
        if (canId == 0x1A0) {             // Handle the first message here
         Serial.println("Received Message with ID 0x1A0:");
@@ -252,6 +238,7 @@ void loop()
   previousTime = currentTime;   // set previous time for next timer loop
   }
 } //if currentTime-previousTime if loop end
+ 
 } //loop end
 
 //CRC16 Generation
@@ -272,3 +259,8 @@ uint16_t crc16_xmodem(const uint8_t *data, size_t len) {
     
   return crc;
 }
+
+
+
+
+  

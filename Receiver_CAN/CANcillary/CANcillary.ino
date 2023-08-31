77060b33d5f6d5ad219afd8cd71b2618661bf198
Max Juhtanovs <juhtanovs.m@gmail.com>
2023-08-31 18:47:33 +0100

    eod commit
    module now responds as should + added conditional signal change for VehicleStateIndicators based

--- Receiver_CAN/CANcillary/CANcillary.ino
+++ Receiver_CAN/CANcillary/CANcillary.ino
@@ -5,26 +5,22 @@
 #define CAN_500KBPS 16     // CAN baud rate
 #define SPI_CS_PIN  17     // setup chip select pin for CAN module
 #define LED 23              //LED pin to show program active
-
-
-#define IND_L_CONT A2       // ADC for steer pot 2 in dbc - red on breadboard
-#define MAIN_BEAM A1         /// Main Beam 
-#define IND_R_CONT A0        // ADC for Throt 3 in dbc - green on breadboard
-#define DIPPED_BEAM 4     //  Dipped Beam
-#define SIDE_LIGHTS 21    // Side Lights
-#define HORN 12            // SW1 input
+#define IND_L_CONT A2       // 2 in dbc - red on breadboard
+#define IND_R_CONT A0        // 3 in dbc - green on breadboard
+#define FLSENSE 12            // SW1 input
 #define FRSENSE 6             // SW2 input
-
-
+#define CH3ADC A1         /// Horn signal 
+#define IND_L_SENSE 4     // Left indicator - 2 in dbc - red on breadboard
+#define IND_R_SENSE 21    // Right indicator - 3 in dbc - green on breadboard
 #define HEADLIGHTS 5      // Headlights input
 #define PROGRAM_MODE false     // set this to enable the serial port (the code will not run if serial enabled but not connected)
 
 MCP_CAN CAN(SPI_CS_PIN);            // Set CS pin
 
 //Messages of interest
-byte StreetDrone_Data_2[8];
-byte StreetDrone_Control_1[8];
-byte Customer_control_2[8];
+byte VehicleStateIndicators[8];
+byte ByWireControlIndicators[8];
+byte DriveByWireState[8];
 
 //Timers
 const unsigned long frameRepetitionTime=5;  // repetition rate of the frame in ms  SET TO 1s for INITIAL TESTS
@@ -50,14 +46,10 @@ unsigned int CH3Input = 0;
 void setup()
 {
   pinMode(LED, OUTPUT);     //Set pins as digital inputs and outputs
-  //pinMode(FLSENSE, INPUT);
-  //pinMode(FRSENSE, INPUT);
+  pinMode(FLSENSE, INPUT);
+  pinMode(FRSENSE, INPUT);
   pinMode(IND_L_CONT, OUTPUT);
   pinMode(IND_R_CONT, OUTPUT);
-  pinMode(MAIN_BEAM, OUTPUT);
-  pinMode(DIPPED_BEAM, OUTPUT);
-  pinMode(SIDE_LIGHTS, OUTPUT);
-  pinMode(HORN, OUTPUT);
   //pinMode(HEADLIGHTS, INPUT);
   //pinMode(INDICATOR_L, INPUT_PULLUP);  //Indicators switch to ground so need a pullup
   //pinMode(INDICATOR_R, INPUT_PULLUP);
@@ -81,14 +73,14 @@ void setup()
   CAN.init_Mask(0, 0, 0x3ff);  // there are 2 masks in mcp2515, you need to set both of them
   CAN.init_Mask(1, 0, 0x3ff);
   
-  CAN.init_Filt(1, 0, 0x100);
-  CAN.init_Filt(2, 0, 0x104);
+  CAN.init_Filt(1, 0, 0x113);
+  CAN.init_Filt(2, 0, 0x1A0);
 }
 
 
 void loop()
 {
-  StreetDrone_Control_1[0];
+  ByWireControlIndicators[0];
   CAN_Error_B = false;
   unsigned long currentMillis = millis();
   //if (0 == millis() % indicator_timer_cal) {
@@ -129,22 +121,22 @@ void loop()
         CAN.readMsgBuf(&len, buf);        // read data,  len: data length, buf: data buf
         unsigned long canId = CAN.getCanId();
       
-        if (canId == 0x100) {             // Handle the second message
+        if (canId == 0x1A0) {             // Handle the second message
         //Serial.println("Received Message with ID 0x1A0:");
         //for (int i = 0; i < len; i++) {
         //  Serial.print(buf[i], HEX);
         //  Serial.print(" ");}
-          StreetDrone_Control_1[0] = buf[0]; 
-          StreetDrone_Control_1[1] = buf[1];
-          StreetDrone_Control_1[2] = buf[2]; 
-          StreetDrone_Control_1[3] = buf[3]; 
-          StreetDrone_Control_1[4] = buf[4]; 
-          StreetDrone_Control_1[5] = buf[5];
-          StreetDrone_Control_1[6] = buf[6];
-          StreetDrone_Control_1[7] = buf[7];
+          DriveByWireState[0] = buf[0]; 
+          DriveByWireState[1] = buf[1];
+          DriveByWireState[2] = buf[2]; 
+          DriveByWireState[3] = buf[3]; 
+          DriveByWireState[4] = buf[4]; 
+          DriveByWireState[5] = buf[5];
+          DriveByWireState[6] = buf[6];
+          DriveByWireState[7] = buf[7];
         
         
-          if (((StreetDrone_Control_1[7] & 0b00000010)>>1) || ((StreetDrone_Control_1[7] & 0b00100000)>>5)){
+          if (DriveByWireState[0] == 3){
           Autonomous_Mode_B = true;
           }
           else {Autonomous_Mode_B = false;}
@@ -158,27 +150,27 @@ void loop()
           
         } 
 
-        if (canId == 0x104) {
+        if (canId == 0x113) {
           //Serial.println("Received Message with ID 0x113:");
           //for (int i = 0; i < len; i++) {
           //Serial.print(buf[i], HEX);
           //Serial.print(" ");
           //}
           
-          Customer_control_2[0] = buf[0]; 
-          Customer_control_2[1] = buf[1];
-          Customer_control_2[2] = buf[2]; 
-          Customer_control_2[3] = buf[3]; 
-          Customer_control_2[4] = buf[4]; 
-          Customer_control_2[5] = buf[5];
-          Customer_control_2[6] = buf[6];
-          Customer_control_2[7] = buf[7];
-          //size_t dataLength1 = sizeof(Customer_control_2);
-          //uint16_t crcResult1 = crc16_xmodem(Customer_control_2, dataLength1);
-          //byte highByte = buf[3];
-          //byte lowByte = buf[2];
-          //unsigned int combinedvalue;
-          //combinedvalue = (highByte << 8 )| lowByte;
+          ByWireControlIndicators[0] = buf[0]; 
+          ByWireControlIndicators[1] = buf[1];
+          ByWireControlIndicators[2] = 0; 
+          ByWireControlIndicators[3] = 0; 
+          ByWireControlIndicators[4] = buf[4]; 
+          ByWireControlIndicators[5] = buf[5];
+          ByWireControlIndicators[6] = buf[6];
+          ByWireControlIndicators[7] = buf[7];
+          size_t dataLength1 = sizeof(ByWireControlIndicators);
+          uint16_t crcResult1 = crc16_xmodem(ByWireControlIndicators, dataLength1);
+          byte highByte = buf[3];
+          byte lowByte = buf[2];
+          unsigned int combinedvalue;
+          combinedvalue = (highByte << 8 )| lowByte;
 
 
           //can_rc_time_ms_113 = millis();
@@ -188,37 +180,23 @@ void loop()
 			  	//CAN_Error_B = false;
 			    //}
 
-
-          if(((Customer_control_2[5] & 0b00000001)>>0) && Autonomous_Mode_B==true){
-            digitalWrite(MAIN_BEAM, HIGH);
-          }
-          if (((Customer_control_2[5] & 0b00000010)>>1) && Autonomous_Mode_B==true){
-            digitalWrite(DIPPED_BEAM,HIGH);
-          }
-          if (((Customer_control_2[5] & 0b00000100)>>2) && Autonomous_Mode_B==true){
-            digitalWrite(SIDE_LIGHTS,HIGH);
-          }
-          if(((Customer_control_2[5] & 0b00001000)>>4) && Autonomous_Mode_B==true){
+          if(ByWireControlIndicators[0] == 2 && Autonomous_Mode_B==true){
             if(Indicator_High_B==true){
-              digitalWrite(IND_L_CONT, LOW);
-              digitalWrite(IND_R_CONT, HIGH);
-              delay(5);
-          
+              digitalWrite(IND_L_CONT, HIGH);
+              digitalWrite(IND_R_CONT, LOW);
               }
             else{digitalWrite(IND_L_CONT, LOW);
             digitalWrite(IND_R_CONT, LOW);}
           } 
-          if(((Customer_control_2[5] & 0b00010000)>>4) && Autonomous_Mode_B==true){
+          else if(ByWireControlIndicators[0] == 3 && Autonomous_Mode_B==true){
             if(Indicator_High_B==true){
-              digitalWrite(IND_R_CONT, LOW);
-              digitalWrite(IND_L_CONT, HIGH);
-              delay(5);
-              
-            }
-            else{digitalWrite(IND_R_CONT, LOW);
-            digitalWrite(IND_L_CONT, LOW);} 
+              digitalWrite(IND_L_CONT, LOW);
+              digitalWrite(IND_R_CONT, HIGH);
+              }
+            else{digitalWrite(IND_L_CONT, LOW);
+            digitalWrite(IND_R_CONT, LOW);}
           } 
-          if(((Customer_control_2[5] & 0b01000000)>>6) && Autonomous_Mode_B==true){
+          else if (ByWireControlIndicators[0] == 4 && Autonomous_Mode_B==true){
             if(Indicator_High_B==true){
               digitalWrite(IND_L_CONT, HIGH);
               digitalWrite(IND_R_CONT, HIGH);}
@@ -226,18 +204,12 @@ void loop()
              digitalWrite(IND_L_CONT, LOW);
              digitalWrite(IND_R_CONT, LOW);}
           } 
-          if(((Customer_control_2[5] & 0b10000000)>>7) && Autonomous_Mode_B==true){
-            digitalWrite(HORN, HIGH);
-          }
           else {
-            digitalWrite(MAIN_BEAM, LOW);
-            digitalWrite(DIPPED_BEAM, LOW);
-            digitalWrite(SIDE_LIGHTS, LOW);
             digitalWrite(IND_L_CONT, LOW);
             digitalWrite(IND_R_CONT, LOW);
-            digitalWrite(HORN, LOW);
           } // End of actions on 0x113 signals
           //Serial.print(ByWireControlIndicators[0]);
+          
         } // End of message 0x113 extraction
 
        
@@ -248,9 +220,9 @@ void loop()
       //Serial.println(CAN.getCanId(),HEX);
       //Serial.print("Buffer value: ");
       
-      //Serial.print("indicatorcommand Value: ");
-      //Serial.print(indicatorcommand, DEC);
-      //Serial.println();
+      Serial.print("indicatorcommand Value: ");
+      Serial.print(ByWireControlIndicators[0], DEC);
+      Serial.println();
 
       //Serial.print("\t");  
       //for(int i = 0; i<1; i++)    // print the data
@@ -282,19 +254,34 @@ void loop()
  
 
   //CYCLIC STATE MESSAGE
-    StreetDrone_Data_2[0] = 0; 
-    StreetDrone_Data_2[1] = count++;
-    StreetDrone_Data_2[2] = 0;
-    StreetDrone_Data_2[3] = 0;
-    StreetDrone_Data_2[4] = 0;
-    StreetDrone_Data_2[5] = 0;  
-    StreetDrone_Data_2[6] = 0;
-    StreetDrone_Data_2[7] = 0;
-    //size_t dataLength = sizeof(VehicleStateIndicators);
-    //uint16_t crcResult = crc16_xmodem(VehicleStateIndicators, dataLength);
-    //VehicleStateIndicators[2] = byte(crcResult & 0x00ff);
-    //VehicleStateIndicators[3] = byte((crcResult >> 8) & 0x00ff); 
-    CAN.sendMsgBuf(0x117, 0, 8, StreetDrone_Data_2);
+    VehicleStateIndicators[0] = 0;
+    if (digitalRead(IND_L_CONT) == false && digitalRead(IND_R_CONT) == false){
+      VehicleStateIndicators[0] = 1;
+    } 
+    else if (digitalRead(IND_L_CONT) == true && digitalRead(IND_R_CONT) == false){
+      VehicleStateIndicators[0] = 2;
+    }
+    else if (digitalRead(IND_L_CONT) == false && digitalRead(IND_R_CONT) == true){
+      VehicleStateIndicators[0] = 3;
+    }
+    else if (digitalRead(IND_L_CONT) == true && digitalRead(IND_R_CONT) == true){
+      VehicleStateIndicators[0] = 4;
+    }
+    else {
+      VehicleStateIndicators[0] = 0;
+    }
+    VehicleStateIndicators[1] = count++;
+    VehicleStateIndicators[2] = 0;
+    VehicleStateIndicators[3] = 0;
+    VehicleStateIndicators[4] = 0;
+    VehicleStateIndicators[5] = 0;  
+    VehicleStateIndicators[6] = 0;
+    VehicleStateIndicators[7] = 0;
+    size_t dataLength = sizeof(VehicleStateIndicators);
+    uint16_t crcResult = crc16_xmodem(VehicleStateIndicators, dataLength);
+    VehicleStateIndicators[2] = byte(crcResult & 0x00ff);
+    VehicleStateIndicators[3] = byte((crcResult >> 8) & 0x00ff); 
+    CAN.sendMsgBuf(0x102, 0, 8, VehicleStateIndicators);
 
     //previousTime = currentTime;   // set previous time for next timer loop
 
